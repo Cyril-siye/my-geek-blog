c58a85fe99124e89beb96218dd916bd09ca0bee9
Cyril-siye <laotou0504@gmail.com>
2026-07-31 04:58:32 +0800

    feat: personalize site info, home page, and add initial post

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Cyril\'s Geek Blog';
+export const SITE_DESCRIPTION = '记录 Linux 系统运维、后端开发与网络技术的个人博客';
